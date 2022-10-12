3ee400d2c93c2e4bffb14e3007008c6026ce43db
Ibrahim Yusuf <ibrobk@gmail.com>
2022-10-12 17:58:55 +0100

    First Commit

---
+++ camera.py
@@ -0,0 +1,11 @@
+import requests
+import numpy as np
+import cv2
+
+while True:
+    images = requests.get("http://192.168.43.1:8080/shot.jpg")
+    video = np.array(bytearray(images.content), dtype=np.uint8)
+    render = cv2.imdecode(video, -1)
+    cv2.imshow('frame', render)
+    if (cv2.waitKey(1) and 0xFF==ord('q')):
+        break
